6612d2e389961f2dbc6d8d29e1ee10bf40b0a4a0
AlexandrMoiseenko <moiseenko.112@mail.ru>
2022-12-05 22:29:52 +0300

    программа по воозведению в степень

---
+++ Ex_001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа
+// (А и В) и возводит число А в натуральную степень В.
+// Реализовать через функции.
+
+int Hello(string message)                           // Метод вывода приглашения и ввода числа
+{
+    Console.Write(message);                  // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine());     // Ввод числа
+}
+
+int Degree(int numberA, int numberB)
+{
+    int count = 1;
+    int result = numberA;
+    while(count < numberB)
+    {
+        result = result * numberA; 
+        count++;
+    }
+    return result;
+}
+
+int numberA = Hello("Введите число А: ");
+int numberB = Hello("Введите число B: ");
+int result = Degree(numberA, numberB);
+
+Console.WriteLine($"Если возвести число А = {numberA} в натуральную степень числа В = {numberB}, то получится = {result} ");
