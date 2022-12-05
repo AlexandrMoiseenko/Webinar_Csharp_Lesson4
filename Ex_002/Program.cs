e4023501e0f9bb81ed45c8bdfdbfea59b63ee52d
AlexandrMoiseenko <moiseenko.112@mail.ru>
2022-12-05 22:31:54 +0300

    программа по вычислению суммы числа

---
+++ Ex_002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и
+// выдает сумму цифр в числе. Реализовать через функции.
+
+int Hello(string message)                             // Метод вывода приглашения и ввода числа
+{
+    Console.Write(message);                          // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine());      // Ввод числа
+}
+
+int Sum(int number)
+{
+   int sum = 0;
+   for(int i = 1; i <= number; i ++)
+   {
+    sum += i; 
+   }
+    return sum;
+}
+
+int number = Hello("Введите число: ");
+int sum = Sum(number);
+
+Console.WriteLine($"Сумма цифр в числе {number} равна {sum}");
